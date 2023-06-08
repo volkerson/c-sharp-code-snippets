e0f1a89f2ee319a252291e68705d5b10372bccae
Ben <126400171+volkerson@users.noreply.github.com>
2023-06-09 01:00:50 +0200

    Create ConsoleApp.cs

---
+++ how-often-is-num-a-dividable-by-num-b/ConsoleApp.cs
@@ -0,0 +1,37 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int result = howOftenIsNumDividable(438, 19);
+            Console.WriteLine($"438 can be divided by 19 {result} times.");
+
+            Console.ReadLine();
+        }
+
+        static int howOftenIsNumDividable(int firstNum, int secondNum)            
+        {
+            int counter = 0;
+            while (firstNum >= 1)
+            {
+                firstNum -= secondNum;
+                if (firstNum >= 1)
+                {
+                    counter++;
+                }
+            }
+
+            return counter;
+        }
+
+    }
+}
